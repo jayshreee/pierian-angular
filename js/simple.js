8bdf7b465c98a49e366c543a29023e59d2c9667b
aniruddha <aniruddha.kudalkar@gmail.com>
2019-08-05 11:08:07 +0530

    js : functional programming

--- js/simple.js
+++ js/simple.js
@@ -1,4 +1,20 @@
 let num1 = 10
 let num2 = 20
+num1 = 'a;al;fld'
+num1 = true
+num1 = 78.56
 
-console.log(`Addition is ${num1 + num2}`)
+console.log(`
+    Addition is ${num1 + num2}
+    afd;;lsa;g4
+    ;s;sf;slflg
+    s'g,,s;fgl
+    s'fl;g'
+    s'ggls'l
+`)
+
+let cost = 20
+if(true) {
+    let cost = 30
+}
+console.log(`Cost is ${cost}`)
