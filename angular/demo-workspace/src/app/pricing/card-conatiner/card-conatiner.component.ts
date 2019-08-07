77988fc3a5c773713b6db0592dbae6dda79fb8c7
aniruddha <aniruddha.kudalkar@gmail.com>
2019-08-07 15:43:25 +0530

    components : custom input

--- angular/demo-workspace/src/app/pricing/card-conatiner/card-conatiner.component.ts
+++ angular/demo-workspace/src/app/pricing/card-conatiner/card-conatiner.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CardData } from './card/card-data';
 
 @Component({
   selector: 'app-card-conatiner',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardConatinerComponent implements OnInit {
 
+  cards: Array<CardData>
+
   constructor() { }
 
   ngOnInit() {
+    this.cards = [
+      { header: 'Free', price: 'Rs.56/mo', features: ['1GG IO', '3 Email', 'Limited Storage'] },
+      { header: 'Blaze', price: 'Rs.90/mo', features: ['8GG IO', '27 Email', 'Unlimited Storage'] },
+      { header: 'Spark', price: 'Rs.190/mo', features: ['50GG IO', '89 Email', 'Unlimited Storage'] }
+    ]
   }
-
 }
+
+
--- angular/demo-workspace/src/app/pricing/card-conatiner/card/card.component.ts
+++ angular/demo-workspace/src/app/pricing/card-conatiner/card/card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CardData } from './card-data';
 
 @Component({
   selector: 'app-card',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
+  @Input()
+  data: CardData
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSignUp() {
+    console.log(`Sign Up Clicked for ${this.data.header}`)
   }
 
 }
