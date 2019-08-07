f33f4728535b9ba5d2d1608169813858e99df32c
aniruddha <aniruddha.kudalkar@gmail.com>
2019-08-07 11:53:32 +0530

    directives : strcutural directives

--- angular/demo-workspace/src/app/directives/directives.component.ts
+++ angular/demo-workspace/src/app/directives/directives.component.ts
@@ -8,12 +8,36 @@ import { Component, OnInit } from '@angular/core';
 export class DirectivesComponent implements OnInit {
 
   st = {
-    background : 'red',
-    color : 'white'
+    background: 'red',
+    color: 'white'
   }
+  isVs = true
+
+  os: Array<OsInfo> = [
+    new OsInfo('iOS', 10),
+    { os: 'android', version: 9 },
+    { os: 'rim', version: 4 },
+    { os: 'mango', version: 5 },
+    { os: 'windows', version: 10 }
+  ]
+
+  osInfo = {
+    os: 'slgklkjslkgjslkg',
+    version: 899
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
+}
 
+export class OsInfo {
+
+  constructor(
+    public os: string,
+    public version: number
+  ) {
+
+  }
 }
