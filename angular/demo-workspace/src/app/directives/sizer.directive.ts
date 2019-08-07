a240e60fafcc881609615f5410d288c5989bb488
aniruddha <aniruddha.kudalkar@gmail.com>
2019-08-07 10:24:27 +0530

    attribute directive : input to directive

--- angular/demo-workspace/src/app/directives/sizer.directive.ts
+++ angular/demo-workspace/src/app/directives/sizer.directive.ts
@@ -1,22 +1,37 @@
-import { Directive, HostBinding, HostListener } from '@angular/core';
+import { Directive, HostBinding, HostListener, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[appSizer]'
 })
-export class SizerDirective {
+export class SizerDirective implements OnInit{
+
+  @Input('appSizer')
+  config : {}
 
   @HostBinding('style.width.px')
   width: number
 
+  @HostBinding('style.background-color')
+  bkCl : string
+
+  @HostBinding('style.color')
+  fgCl : string
+
   constructor() { }
+
+  ngOnInit() {
+    this.width = this.config['wd']
+    this.bkCl = this.config['bkCl']
+    this.fgCl = this.config['fgCl']
+  }
 
   @HostListener('mouseenter')
   mouseIn() {
-    this.width = 500
+    this.width = this.width + 50
   }
 
   @HostListener('mouseleave')
   mouseOut() {
-    this.width = 300
+    this.width = this.width - 50
   }
 }
